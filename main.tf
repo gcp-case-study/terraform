0e9e5f2a0172a847695e368918e8b0f6f9ac3b94
jackson218 <wbjackson218@gmail.com>
2019-08-29 16:01:35 -0400

    created appEngine.tf and added 2 more projects to main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ provider "google" {
   project     = "my-project-id"
   region      = "us-central1"
 }
+
+provider "google" {
+  version = "~> 2.13"
+  project     = "my-project-id2"
+  region      = "us-central1"
+}
+
+provider "google" {
+  version = "~> 2.13"
+  project     = "my-project-id3"
+  region      = "us-central1"
+}
