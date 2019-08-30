902b61e9d75a0c8b747c707c17a59062b88e2748
EricBAllen <49167218+EricBAllen@users.noreply.github.com>
2019-08-30 09:27:50 -0400

    Add files via upload

--- iam.tf
+++ iam.tf
@@ -1,37 +1,67 @@
-data "google_iam_role" "roleinfo" {
-  name = "roles/compute.viewer"
-}
 
 data "google_iam_policy" "admin" {
+# create role for compute admin
   binding { # required for google_iam_policy
-    role = "roles/compute.instanceAdmin" # required for "binding"
-
-    members = [ # required for "binding"
+    role = "roles/compute.admin" 
+  
+    members = [ 
       "user:tekken7jinkazama09@gmail.com",
     ]
   }
-
+# create role for compute_instance
   binding {
-    role = "roles/storage.objectViewer"
-
+    role = "roles/compute.instanceAdmin" # Permissions to create, modify, 
+    # and delete virtual machine instances
     members = [
-      "user:ericallen740@yahoo.com",
+      "user:tekken7jinkazama09@gmail.com"
     ]
   }
 
-  audit_config { # optional 
-    service = "cloudsql.googleapis.com" # required for "audit_config"
-    audit_log_configs { # required for "audit_config"
-      log_type = "ADMIN_READ", # required for "audit_config"
-      exempted_members = ["user:Matt_Shwartz@gmail.com"] # optional for "audit_config"
-    }
-
-    audit_log_configs {
-      log_type = "DATA_WRITE",
-    }
-
-    audit_log_configs {
-      log_type = "ADMIN_READ",
-    }
+# create role for loadbalancer
+  binding {
+    role = "roles/compute.loadBalancerAdmin" # Permissions to create, modify, 
+    # and delete load balancers and associate resources
+    members = [
+      "user:tekken7jinkazama09@gmail.com"
+    ]
   }
+
+# create role for network
+  binding {
+    role = "roles/compute.networkAdmin" # Permissions to create, modify, 
+# and delete networking resources, except for firewall rules and SSL certificates
+    members = [
+      "user:tekken7jinkazama09@gmail.com"
+    ]
+  }
+# create role for security policies
+    binding {
+      role = "roles/compute.orgSecurityPolicyAdmin" # Full control of Compute 
+# Engine Organization Security Policies
+      members = [
+        "user:tekken7jinkazama09@gmail.com"
+      ]
+    }
+# create role for storage admin
+    binding {
+      role = "roles/compute.storageAdmin"
+      members = [
+        "user:tekken7jinkazama09@gmail.com"
+      ]
+    }
+# create role for service accounts
+    binding {
+      role = "roles/iam.serviceAccountAdmin" # Create and manage service accounts.
+      members = [
+        "user:tekken7jinkazama09@gmail.com"
+      ]
+    }
+
+# create role for account keys
+    binding {
+      role = "roles/iam.serviceAccountKeyAdmin" # Create and manage (and rotate) service account keys.
+      members = [
+        "user:tekken7jinkazama09@gmail.com"
+      ]
+    }
 }
